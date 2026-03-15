1ec0b604792c5167b01e55d35c4bc5e371a2838a
shyamnakka <nakkashyam205@gmail.com>
2026-03-15 22:55:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (41.17%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int total=n*(n+1)/2;
+        int x=sqrt(total);
+        if(x*x==total) return x;
+        else return -1;
+        
+    }
+};
