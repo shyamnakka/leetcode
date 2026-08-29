0c8829a835923d0080a65e5e9fc7e3809e3b2255
shyamnakka <nakkashyam205@gmail.com>
2026-08-29 23:27:22 +0530

    Time: 2 ms (38.75%) | Memory: 8.8 MB (48.13%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -3,28 +3,29 @@ public:
     string shortestBeautifulSubstring(string s, int k) {
         int n=s.size();
         int mini=INT_MAX;
+        int l=0,r=0,ones=0;
         string ans;
-        for(int i=0;i<n;i++){
-            int ones=0;
-            string curr;
-            for(int j=i;j<n;j++){
-                curr=curr+s[j];
-                if(s[j]=='1') ones++;
-                if(ones==k){
-                    int size=j-i+1;
-                    if(mini>size){
+        while(r<n){
+            if(s[r]=='1') ones++;
+            while(ones==k){
+                int len=r-l+1;
+                if(mini>len){
+                    mini=len;
+                    ans=s.substr(l,len);
+                }
+                else if(mini==len){
+                    string curr=s.substr(l,len);
+                    if(ans>curr){
                         ans=curr;
-                        mini=size;
-                    }
-                    if(mini==size){
-                        if(ans>curr){
-                            ans=curr;
-                        }
                     }
                 }
+                if(s[l]=='1') ones--;
+                l++;
             }
+            r++;
         }
         return ans;
+        
         
     }
 };
